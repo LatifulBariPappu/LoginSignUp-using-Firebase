cec38b342222ea8cfb22d51faa25023100013902
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-08-23 00:51:04 +0600

    Login backend

--- app/src/main/java/com/example/signuploginwithrealtimedatabase/LoginActivity.java
+++ app/src/main/java/com/example/signuploginwithrealtimedatabase/LoginActivity.java
@@ -2,13 +2,44 @@ package com.example.signuploginwithrealtimedatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private EditText loginEmail,loginPassword;
+    private Button loginButton;
+    private TextView signUpRedirectText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth=FirebaseAuth.getInstance();
+        loginEmail=findViewById(R.id.login_email);
+        loginPassword=findViewById(R.id.login_password);
+        loginButton=findViewById(R.id.login_btn);
+        signUpRedirectText=findViewById(R.id.signupRedirextText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user=loginEmail.getText().toString().trim();
+                String pass=loginPassword.getText().toString().trim();
+            }
+        });
+        signUpRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SingUpActivity.class));
+            }
+        });
     }
 }
