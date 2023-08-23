072cd2f038e599a0befaaaf2c1ca5846aee21d59
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-08-23 13:06:15 +0600

    authentication is successful

--- app/src/main/java/com/example/signuploginwithrealtimedatabase/LoginActivity.java
+++ app/src/main/java/com/example/signuploginwithrealtimedatabase/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.example.signuploginwithrealtimedatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,8 +38,36 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user=loginEmail.getText().toString().trim();
+                String email=loginEmail.getText().toString().trim();
                 String pass=loginPassword.getText().toString().trim();
+
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if(!pass.isEmpty()){
+                        auth.signInWithEmailAndPassword(email,pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }else{
+                        loginPassword.setError("Password cannot be empty");
+
+                    }
+                    if (email.isEmpty()) {
+                        loginEmail.setError("Email cannot be empty");
+                    }else{
+                        loginEmail.setError("Please enter valid email");
+                    }
+
+                }
             }
         });
         signUpRedirectText.setOnClickListener(new View.OnClickListener() {
