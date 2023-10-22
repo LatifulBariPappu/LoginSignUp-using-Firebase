46bdacd9303a6cc2a3147c7ecec573637ce5a34d
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-10-22 14:48:08 +0600

    google signin

--- app/src/main/java/com/example/signuploginwithrealtimedatabase/LoginActivity.java
+++ app/src/main/java/com/example/signuploginwithrealtimedatabase/LoginActivity.java
@@ -1,10 +1,17 @@
 package com.example.signuploginwithrealtimedatabase;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -12,8 +19,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.developer.gbuttons.GoogleSignInButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,6 +38,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText loginEmail,loginPassword;
     private Button loginButton;
     private TextView signUpRedirectText;
+    GoogleSignInButton googleSignInButton;
+    GoogleSignInOptions gOptions;
+    GoogleSignInClient gClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +50,8 @@ public class LoginActivity extends AppCompatActivity {
         loginPassword=findViewById(R.id.login_password);
         loginButton=findViewById(R.id.login_btn);
         signUpRedirectText=findViewById(R.id.signupRedirextText);
+        googleSignInButton=findViewById(R.id.googleBtn);
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +91,39 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this,SingUpActivity.class));
+            }
+        });
+        gOptions=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gClient= GoogleSignIn.getClient(this,gOptions);
+        GoogleSignInAccount gAccount=GoogleSignIn.getLastSignedInAccount(this);
+        if(gAccount!=null){
+            finish();
+            Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+        ActivityResultLauncher<Intent> activityResultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult o) {
+                if(o.getResultCode()== Activity.RESULT_OK){
+                    Intent data=o.getData();
+                    Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+                    try{
+                        task.getResult(ApiException.class);
+                        finish();
+                        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }catch (ApiException e){
+                        Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent=gClient.getSignInIntent();
+                activityResultLauncher.launch(signInIntent);
             }
         });
     }
